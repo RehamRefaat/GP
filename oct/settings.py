28ddd34efeedc30bbdfc6bb5d4c396563dff7d10
RehamRefaat <loleshzaki@gmail.com>
2023-06-26 14:42:13 +0300

    Add Static

--- oct/settings.py
+++ oct/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 "New Change By Reham line2"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,14 +130,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+"""
 STATIC_URL = '/static/'
-
-
+STATICFILES_DIRS = ['static/']
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '/static/'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')"""
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
