ad80c2fe7717f70c72fe834ac90fbf690f63e006
RehamRefaat <loleshzaki@gmail.com>
2023-06-26 05:45:54 +0300

    Add New Changes

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,2 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import *
-urlpatterns = [
-    path('home/', home_page,name='home'),
-    path('about/', about_page,name='about'),
-    path('services/', services_page,name='services'),
-    path('contact/', contact_page,name='contact'),
-]
