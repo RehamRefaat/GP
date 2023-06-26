a617b73486e2f7f529fab312192b710e4bd3ad76
RehamRefaat <loleshzaki@gmail.com>
2023-06-26 15:42:13 +0300

    Add New Change

--- oct/settings.py
+++ oct/settings.py
@@ -132,25 +132,21 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-"""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
 
-
+"""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['static/']
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '/static/'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+"""
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')"""
-STATIC_ROOT = BASE_DIR / 'static'
-
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
